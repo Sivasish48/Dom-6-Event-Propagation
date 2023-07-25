403fb034d3de5a7bbc5a47977bbe18b2fc315933
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2023-07-25 09:15:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// 1. UNDERSTANDING EVENT PROPAGATION
+// Event propagation is a mechanism that defines how events propagate or travel through the DOM tree to arrive at its target and what happens to it afterward.
+
+
+
+// 1.1 Event Propagation includes 3 phases i.e
+// Event Capturing , Target & Event Bubbling. 
+
+// 1.2 EVENT  CAPTURING
+
+// This capturing phase starts from the root and travels all the way to the target.
+// SEE the image to learn more
+
+// 1.3 TARGET 
+
+// This is the phase where the journey all the way to taget is finished .
+
+// 1.4 EVENT BUBBLING
+
+//  In the EVENT BUBBLING PHASE it travels back to the root from the target in DOM. It is basically the opposite of EVENT CAPTURING.
